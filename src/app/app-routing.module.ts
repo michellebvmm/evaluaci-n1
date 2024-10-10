047f8737da9c88e3d072025a9192f18572248224
michellebvmm <michelle.1807bm@gmail.com>
2024-10-10 12:40:39 -0600

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule)
+  },
+  {
+    path: 'activity',
+    loadChildren: () => import('./pages/activity/activity.module').then(m => m.ActivityPageModule)
+  },
+  {
+    path: 'settings',
+    loadChildren: () => import('./pages/settings/settings.module').then(m => m.SettingsPageModule)
+  },
+  {
+    path: 'add-activity',
+    loadChildren: () => import('./pages/add-activity/add-activity.module').then( m => m.AddActivityPageModule)
+  },
+  {
+    path: 'add-activity',
+    loadChildren: () => import('./pages/add-activity/add-activity.module').then(m => m.AddActivityPageModule)
+  }
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
